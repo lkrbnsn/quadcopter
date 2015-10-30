af711c97dd670a5315372a73330d08cd37dd9366
lkrbnsn <luke.nenagh@gmail.com>
2015-10-30 23:37:40 +0000

    New IMU code, using PID, got roll to balance

--- quadcopter.ino
+++ quadcopter.ino
@@ -1,9 +1,10 @@
 #include <Servo.h>
+#include <PID_v1.h>
+
+#include "freeram.h"
+
+#include "mpu.h"
 #include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
 
 #define MAX_SPEED 80
 #define MIN_SPEED 50
@@ -18,216 +19,119 @@
   black
 */
 
-//Servo motor1;
-//Servo motor2;
+double roll = 0;
+double rollRads = 0;
+
+Servo motor1;
+Servo motor2;
 Servo motor3;
 Servo motor4;
 
-float v3 = 60;
-float v4 = 60;
+float v3 = 65;
+float v4 = 65;
 
-MPU6050 mpu;
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
+// PID variables
+double Setpoint, Input, Output;
+double Kp=0.07, Ki=0.03, Kd=0.04;
+//double Kp=0.05, Ki=0.01, Kd=0.04;
+PID myPID(&roll, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-float yprOffset[] = {0, 0, 0};
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady()
-{
-  mpuInterrupt = true;
-}
-
-void setupMpu()
-{
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
-
-  // initialize serial communication
-  // (115200 chosen because it is required for Teapot Demo output, but it's
-  // really up to you depending on your project)
-  Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-  // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
-
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // wait for ready
-  //  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  //  while (Serial.available() && Serial.read()); // empty buffer
-  //  while (!Serial.available());                 // wait for data
-  //  while (Serial.available() && Serial.read()); // empty buffer again
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(0);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(0, dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
-
-  // configure LED for output
-  pinMode(LED_PIN, OUTPUT);
-}
+int ret;
+unsigned long startTime;
 
 void calculateVelocities()
 {
-  v3 = v3 - ypr[2];
-  v4 = v4 + ypr[2];
+  myPID.Compute();
+//  Serial.println(Output);
+  
+//  v3 = Output * 0.02;
+//  v4 = Output * 0.02;
+  v3 = 70 + (-Output);
+  v4 = 70 - (-Output);
 
   if (v3 > MAX_SPEED)
     v3 = MAX_SPEED;
   if (v3 < MIN_SPEED)
     v3 = MIN_SPEED;
-    
+
   if (v4 > MAX_SPEED)
     v4 = MAX_SPEED;
   if (v4 < MIN_SPEED)
     v4 = MIN_SPEED;
-    
-  Serial.println("calc");
 }
 
 void writeToMotors()
 {
+//  Serial.print(v3);
+//  Serial.print("\t");
+//  Serial.print(v4);
+//  Serial.print("\n");
+  
   motor3.write(v3);
   motor4.write(v4);
+  motor2.write(v3);
+  motor1.write(v4);
 }
 
+void setup() {
+    Fastwire::setup(400,0);
+    Serial.begin(115200);
+    ret = mympu_open(2000);
+    Serial.print("MPU init: "); Serial.println(ret);
+    Serial.print("Free mem: "); Serial.println(freeRam());
 
-///////////////////////////////////////////////////
-
-
-void setup()
-{
-  //  motor1.attach(6);
-  //  motor2.attach(7);
-  motor3.attach(8);
-  motor4.attach(9);
-
-  setupMpu();
-  //  Serial.println("Calibrating...");
-  //  delay(5000);
-  //  yprOffset[1] = yprOffset[1] * 9 + ypr[
-  //  Serial.println("Done calibrating");
+    motor1.attach(6);
+    motor2.attach(7);
+    motor3.attach(8);
+    motor4.attach(9);
+    
+    Setpoint = 0;
+    myPID.SetOutputLimits(-200, 200);
+    myPID.SetMode(AUTOMATIC);
 }
 
-
-////////////////////////////////////////////////////
-
+unsigned int c = 0; //cumulative number of successful MPU/DMP reads
+unsigned int np = 0; //cumulative number of MPU/DMP reads that brought no packet back
+unsigned int err_c = 0; //cumulative number of MPU/DMP reads that brought corrupted packet
+unsigned int err_o = 0; //cumulative number of MPU/DMP reads that had overflow bit set
 
 void loop() {
-  // if programming failed, don't try to do anything
-  if (!dmpReady) return;
+    startTime = micros();
 
-  // wait for MPU interrupt or extra packet(s) available
-  while (!mpuInterrupt && fifoCount < packetSize) {
+    ret = mympu_update();
+
+    switch (ret) {
+	case 0: c++; break;
+	case 1: np++; return;
+	case 2: err_o++; return;
+	case 3: err_c++; return; 
+	default: 
+		Serial.print("READ ERROR!  ");
+		Serial.println(ret);
+		return;
+    }
+
+//    if (!(c%25)) {
+//	    Serial.print(np); Serial.print("  "); Serial.print(err_c); Serial.print(" "); Serial.print(err_o);
+//	    Serial.print(" Y: "); Serial.print(mympu.ypr[0]);
+//	    Serial.print(" P: "); Serial.print(mympu.ypr[1]);
+//	    Serial.print(" R: "); Serial.println(mympu.ypr[2]);
+//    }
+
+    if(mympu.ypr[2] > 0)
+      roll = mympu.ypr[2] - 175;
+    else
+      roll = 185 + mympu.ypr[2];
+    
+//    rollRads = roll * 0.017444; // convert to rads
+//    roll = abs(roll) * sin(rollRads);
+      
+    Serial.print(" R: "); Serial.println(roll);
     
     calculateVelocities();
     writeToMotors();
-    
-  }
 
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
+    Serial.println(micros() - startTime);
 
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    Serial.println("Int");
-//    Serial.print("ypr\t");
-//    Serial.print(ypr[0] * 180 / M_PI);
-//    Serial.print("\t");
-//    Serial.print(ypr[1] * 180 / M_PI - 3.4);
-//    Serial.print("\t");
-//    Serial.println(ypr[2] * 180 / M_PI - 13.3);
-
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
-  }
 }
+
