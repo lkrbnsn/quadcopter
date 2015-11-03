184fd904996a71098d1e57de8c89d7c90da233f5
lkrbnsn <luke.nenagh@gmail.com>
2015-11-03 20:50:56 +0000

    Pitch PID connected to motors

--- quadcopter.ino
+++ quadcopter.ino
@@ -6,7 +6,7 @@
 #include "mpu.h"
 #include "I2Cdev.h"
 
-#define MAX_SPEED 80
+#define MAX_SPEED 100
 #define MIN_SPEED 50
 
 /*
@@ -31,19 +31,19 @@ float v2 = 0;
 float v3 = 0;
 float v4 = 0;
 
-// Roll PID variables
-double roll = 0;
-double rSetpoint = 0;
-double rOutput;
-double rKp = 0.07, rKi = 0.03, rKd = 0.04;
-PID rPID(&roll, &rOutput, &rSetpoint, rKp, rKi, rKd, DIRECT);
-
 // Pitch PID variables
 double pitch = 0;
 double pSetpoint = 0;
 double pOutput;
 double pKp = 0.07, pKi = 0.03, pKd = 0.04;
 PID pPID(&pitch, &pOutput, &pSetpoint, pKp, pKi, pKd, DIRECT);
+
+// Roll PID variables
+double roll = 0;
+double rSetpoint = 0;
+double rOutput;
+double rKp = 0.07, rKi = 0.03, rKd = 0.04;
+PID rPID(&roll, &rOutput, &rSetpoint, rKp, rKi, rKd, DIRECT);
 
 int ret;
 unsigned long startTime;
@@ -52,12 +52,14 @@ int throttle = 0;
 
 void calculateVelocities()
 {
-  rPID.Compute();
   pPID.Compute();
-  //  Serial.println(Output);
+  rPID.Compute();
+  Serial.println(rOutput);
 
-  v3 = 55 + (10 * throttle) - rOutput;
-  v4 = 55 + (10 * throttle) + rOutput;
+  v2 = 60 + throttle - pOutput - rOutput;
+  v1 = 60 + throttle - pOutput + rOutput;
+  v3 = 60 + throttle + pOutput - rOutput;
+  v4 = 60 + throttle + pOutput + rOutput;
 
   if (v3 > MAX_SPEED)
     v3 = MAX_SPEED;
@@ -95,11 +97,11 @@ void setup() {
   motor3.attach(8);
   motor4.attach(9);
 
-  rPID.SetOutputLimits(-200, 200);
-  rPID.SetMode(AUTOMATIC);
-
   pPID.SetOutputLimits(-200, 200);
   pPID.SetMode(AUTOMATIC);
+
+  rPID.SetOutputLimits(-200, 200);
+  rPID.SetMode(AUTOMATIC);
 }
 
 unsigned int c = 0; //cumulative number of successful MPU/DMP reads
@@ -112,7 +114,11 @@ void loop() {
   {
     char ch = Serial.read();
     if (ch != 10) {
-      throttle = ch - 48;
+      throttle = (ch - 48) * 4;
+      if (throttle > 30)
+        throttle = 30;
+      if (throttle < 0)
+        throttle = 0;
     }
   }
 
@@ -138,21 +144,18 @@ void loop() {
   //	    Serial.print(" R: "); Serial.println(mympu.ypr[2]);
   //    }
 
+  pitch = mympu.ypr[1] - 6;
+
   if (mympu.ypr[2] > 0)
     roll = mympu.ypr[2] - 175;
   else
     roll = 185 + mympu.ypr[2];
-    
-  if (mympu.ypr[1] > 0)
-    pitch = mympu.ypr[1] - 175;
-  else
-    pitch = 185 + mympu.ypr[1];
 
   //    rollRads = roll * 0.017444; // convert to rads
   //    roll = abs(roll) * sin(rollRads);
 
-  Serial.print(" R: "); Serial.println(roll);
   Serial.print(" P: "); Serial.println(pitch);
+  Serial.print(" R: "); Serial.println(roll);
 
   calculateVelocities();
   writeToMotors();
